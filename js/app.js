bf65c5608362363240256563f20304fd1a0c95a3
HarmanGill <harman97gill@gmail.com>
2024-02-21 22:26:15 -0500

    almost pulling from API

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log('hello')
+window.onload = () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.querySelector('input[type="text"]').value
+
+    fetch('https://rapidapi.com/akshithp111/api/books-api7=' + userInput).then((data) => {
+      return data.json()
+    },
+    (err) => {
+      console.log("Error: ", err)
+    }
+    ).then(
+      (json) => {
+        console.log("JSON DATA: ", json)
+        if(json.Title && json.Author && json.Rating && json.Genres){
+          document.querySelector('#title').innerText = json.Title
+          document.querySelector('#author').innerText = json.Author
+          document.querySelector('#rating').innerText = json.Rating
+          document.querySelector('#genres').innerText = json.Genres 
+        } else {
+          console.log(document.querySelector('#title'))
+          document.querySelector('dl').style.display = 'none'
+          const errorMessage = document.createElement('h1')
+          errorMessage.innerText = "Book Not Found"
+          document.querySelector('body').append(errorMessage)
+        }
+      }
+    )
+  })
+}
