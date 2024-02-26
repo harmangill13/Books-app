a6f8b6c09c9fbc738dfda372af30f5f0c1713495
HarmanGill <harman97gill@gmail.com>
2024-02-26 14:00:32 -0500

    User complex working, API still not

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ window.onload = () => {
         console.log("JSON DATA: ", json)
 
         if(json.Title && json.Author && json.Rating && json.Genres && json.array.length > 0){
-          json.array.forEach((books) => {
+          json.forEach((books) => {
             document.querySelector('#title').innerText = books.Title
             document.querySelector('#author').innerText = books.Author
             document.querySelector('#rating').innerText = books.Rating
@@ -47,5 +47,113 @@ window.onload = () => {
       }
     )
   })
+
+}
+const randomBook = () => {
+  const booksArray =  [
+  {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960
+  },
+  {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949
+  },
+  {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      year: 1925
+  },
+  {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      year: 1813
+  },
+  {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      year: 1951
+  },
+  {
+      title: "Brave New World",
+      author: "Aldous Huxley",
+      year: 1932
+  },
+  {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      year: 1937
+  },
+  {
+      title: "Animal Farm",
+      author: "George Orwell",
+      year: 1945
+  },
+  {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      year: 1954
+  },
+  {
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      year: 1851
+  },
+  {
+      title: "Jane Eyre",
+      author: "Charlotte Brontë",
+      year: 1847
+  },
+  {
+      title: "Wuthering Heights",
+      author: "Emily Brontë",
+      year: 1847
+  },
+  {
+      title: "The Picture of Dorian Gray",
+      author: "Oscar Wilde",
+      year: 1890
+  },
+  {
+      title: "Frankenstein",
+      author: "Mary Shelley",
+      year: 1818
+  },
+  {
+      title: "Dracula",
+      author: "Bram Stoker",
+      year: 1897
+  },
+  {
+      title: "The Adventures of Sherlock Holmes",
+      author: "Arthur Conan Doyle",
+      year: 1892
+  },
+  {
+      title: "The War of the Worlds",
+      author: "H.G. Wells",
+      year: 1898
+  },
+  {
+      title: "Treasure Island",
+      author: "Robert Louis Stevenson",
+      year: 1883
+  },
+  {
+      title: "Alice's Adventures in Wonderland",
+      author: "Lewis Carroll",
+      year: 1865
+  },
+  {
+      title: "Gulliver's Travels",
+      author: "Jonathan Swift",
+      year: 1726
+  }
+]
+  const pickRandomIndex = Math.floor(Math.random() * booksArray.length)
+  
+  return alert(`${booksArray[pickRandomIndex].title}`)
 }
 
